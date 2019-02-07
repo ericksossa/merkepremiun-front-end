1276216fc3a1448b013724693c205f5be48a74dc
erick <ericksossa09@gmail.com>
2019-02-06 20:00:47 -0500

    cambios en el template y en los estilos

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenStorageService } from '../../services/auth/token-storage.service';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  info: any;
+  constructor(private token: TokenStorageService) { }
 
   ngOnInit() {
+    this.info = {
+      token: this.token.getToken(),
+      refreshToken: this.token.getRefreshToken(),
+      username: this.token.getUsername(),
+      // authorities: this.token.getAuthorities()
+    };
   }
 
 }
