52818a57f2f65c0af229ae636b9c616a06b59430
erick <ericksossa09@gmail.com>
2019-03-02 12:51:41 -0500

    listando usuarios y aplicando un descuento a caada uno

--- src/app/pages/admin/customers/customers.component.ts
+++ src/app/pages/admin/customers/customers.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomersService } from 'src/app/services/service.index';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-customers',
@@ -6,10 +8,52 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class CustomersComponent implements OnInit {
+  customers: any[] = [];
 
-  constructor() { }
+  constructor(private customersService: CustomersService) { }
 
   ngOnInit() {
+    this.getCustomers();
   }
+
+  getCustomers() {
+    this.customersService.allCustomers()
+      .subscribe(resp => {
+        this.customers = resp.data;
+        console.log(resp);
+      });
+  }
+
+  onSave(client: any) {
+    this.customersService.updateDiscount(client.id, client)
+      .subscribe(resp => {
+        // bien
+        const Toast = swal.mixin({
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          timer: 4000
+        });
+        Toast.fire({
+          type: 'success',
+          title: `${resp.message}`,
+        });
+
+      }, err => {
+        // error
+        const Toast = swal.mixin({
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          timer: 4000
+        });
+        Toast.fire({
+          type: 'error',
+          title: `${err.error.message}`,
+        });
+      });
+
+  }
+
 
 }
